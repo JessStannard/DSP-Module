65dae777f1248f0e6dca1b84b343d7672ff45bc6
JessStannard <jessica.stannard@icloud.com>
2025-03-11 14:52:52 +0000

    added note function

--- note.m
+++ note.m
@@ -6,7 +6,7 @@ Ts = 1/Fs;
 A4=440;
 ref_key=49;     %A4 is the 49th Key in piano keyboard 
 n = keynum-ref_key;  %Calculate the difference between ref_key and input Key
-freq = A4*2^(n/12); %Calculate the frequency of input Key
+freq = A4*2^(n/ 2); %Calculate the frequency of input Key
 Time = 0:Ts:dur;
 
 noteX=A*sin(2*pi*freq*Time);
